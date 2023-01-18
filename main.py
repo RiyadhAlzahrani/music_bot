cedc9fcad5eeaba5ca17d2f5e87ec7fce8cf4ebf
Riyadh Alzahrani <48260425+RiyadhAlzahrani@users.noreply.github.com>
2023-01-18 10:02:02 +0300

    Code Edits 18-1-2023
    From---
    Line 9: 
    bot = commands.Bot(command_prefix='/')
    
    Line 15 and 16:
    bot.add_cog(help_cog(bot))
    bot.add_cog(music_cog(bot))
    
    Line 19:
    bot.run(os.getenv("TOKEN"))
    
    To---
    Line 9: 
    bot = commands.Bot(intents=discord.Intents.all() , command_prefix='/' , description='The Best Music-Bot For the Best User!')
    
    Line 15 and 16:
    @bot.event
    async def on_ready():
        await bot.add_cog(help_cog(bot))
        await bot.add_cog(music_cog(bot))
    
    Line 19:
    with open('Token') as file:
        bot.run(file.read())

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import os
 from help_cog import help_cog
 from music_cog import music_cog
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(intents=discord.Intents.all() , command_prefix='/' , description='The Best Music-Bot For the Best User!')
 
 #remove the default help command so that we can write out own
 bot.remove_command('help')
 
 #register the class with the bot
-bot.add_cog(help_cog(bot))
-bot.add_cog(music_cog(bot))
+@bot.event
+async def on_ready():
+    await bot.add_cog(help_cog(bot))
+    await bot.add_cog(music_cog(bot))
 
-#start the bot with our token
-bot.run(os.getenv("TOKEN"))
+#start the bot with our token by adding the token in the Token File
+with open('Token') as file:
+    bot.run(file.read())
